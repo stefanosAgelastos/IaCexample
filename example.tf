c4bc4df2566678b46b156b3c8c4426bc767a2d1c
stefanos <sagelastos@gmail.com>
2019-09-17 17:59:14 +0200

    removed unused

--- example.tf
+++ example.tf
@@ -6,8 +6,4 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-1b2fa465"
   instance_type = "t3.micro"
-
-  # Tells Terraform that this EC2 instance must be created only after the
-  # S3 bucket has been created.
-  depends_on = [aws_s3_bucket.example]
 }
